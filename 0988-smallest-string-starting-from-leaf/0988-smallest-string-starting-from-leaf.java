9839cef58fed1fc6ffdd9977551d21c6c9cc8695
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-06 16:28:58 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    String answer = "";
+    private String compare(String a, String b) {
+        if(a.length() != b.length()) {
+            if(a.length() < b.length())
+                return a;
+            return b;
+        }
+        if(a.compareTo(b) < 0)
+            return a;
+        return b;
+    }
+    private void tree(StringBuilder sb, TreeNode current) {
+        sb.append((char)('a' + current.val));
+        if(current.left == null && current.right == null) {
+            String candidate = sb.reverse().toString();
+            sb.reverse();
+            if(answer.equals(""))
+                answer = candidate;
+            else
+                answer = compare(answer, candidate);
+            return;
+        }
+        if(current.left != null) {
+            tree(sb, current.left);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        if(current.right != null) {
+            tree(sb, current.right);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        return;
+    }
+    public String smallestFromLeaf(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        tree(sb, root);
+        return answer;
+    }
+}

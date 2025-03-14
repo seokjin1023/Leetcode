b983e2603c5a3e2083341565e85c57977514725a
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-14 15:26:48 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if(grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1)
+            return -1;
+        Queue<int[]> bfs = new LinkedList<>();
+        bfs.add(new int[]{0, 0});
+        grid[0][0] = 1;
+        while(!bfs.isEmpty()) {
+            int[] currentLocation = bfs.poll();
+            int curX = currentLocation[0];
+            int curY = currentLocation[1];
+            int[][] direction = {{-1 , 1}, {0 , 1}, {1 , 1}, {-1 , 0}, {1 , 0}, {-1 , -1}, {0 , -1}, {1 , -1}};
+            for(int i = 0; i < direction.length; i++) {
+                int x = direction[i][0];
+                int y = direction[i][1];
+                if(curX + x >= 0 && curX + x < grid.length && curY + y >= 0 && curY + y < grid.length && grid[curX + x][curY + y] == 0) {
+                    grid[curX + x][curY + y] = grid[curX][curY] + 1;
+                    bfs.add(new int[]{curX + x, curY + y});
+                }
+            }
+        }
+        return grid[grid.length - 1][grid[0].length - 1];
+    }
+}

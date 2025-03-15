d8872b4d661c8132353f4cdc8779595c7be453aa
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-15 21:25:18 +0900

    Time: 3 ms (99.78%), Space: 53.9 MB (96.14%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
@@ -14,6 +14,33 @@
  * }
  */
 class Solution {
+    int answer = 0;
+    private void zigZag(TreeNode node, boolean goRight, int depth) {
+        if(node == null)
+            return;
+        
+        if(depth > answer)
+            answer = depth;
+        
+        if(goRight) {
+            zigZag(node.right, false, depth + 1);
+            zigZag(node.left, true, 1);
+        }
+        else {
+            zigZag(node.left, true, depth + 1);
+            zigZag(node.right, false, 1);
+        }
+    }
+    public int longestZigZag(TreeNode root) {
+        zigZag(root, true, 0);
+        
+        return answer;
+    }
+}
+
+/* 
+Solution with bfs
+class Solution {
     private class Treedepth {
         TreeNode node;
         boolean goRight;
@@ -53,3 +80,4 @@ class Solution {
         return answer;
     }
 }
+*/

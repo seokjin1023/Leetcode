47e52b443db4e1a22b3c74c7128a3bc5c3c4a5aa
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-18 10:11:01 +0900

    Time: 13 ms (15.26%), Space: 58.2 MB (8.45%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.java
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -1,9 +1,9 @@
 //AND 연산을 한 이후 그것을 되돌리고 싶다면 원래 알던 수를 |연산을 통해서 다시 계산하면 되돌아옴.
-//a & b == a + b일 때 둘의 비트가 겹치지 않는 것!
+//a ^ b == a + b일 때 둘의 비트가 겹치지 않는 것!
 class Solution {
     public int longestNiceSubarray(int[] nums) {
         Queue<Integer> que = new LinkedList<>();
-        int answer = 0;
+        int answer = 1;
         int subLength = 1;
         int sum = nums[0];
         que.add(nums[0]);

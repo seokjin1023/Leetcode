f224b83d64e9907a06dfe48917371e12deb8215c
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-21 09:45:29 +0900

    Time: 461 ms (95.09%), Space: 46.6 MB (71.1%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        List<String> answer = new ArrayList<>();
+        Set<String> newIngredients = new HashSet<>();
+        Set<Integer> canNotMake = new HashSet<>();
+        boolean hasNewIngredients = false;
+        for(int i = 0; i < recipes.length; i++) 
+            canNotMake.add(i);
+        for(int i= 0; i < supplies.length; i++) 
+            newIngredients.add(supplies[i]);
+        do {
+            hasNewIngredients = false;
+            Set<Integer> toRemove = new HashSet<>();
+            for(Integer recipeNum : canNotMake) {
+                List<String> currentIngredients = ingredients.get(recipeNum);
+                boolean canMake = true;
+                for(String ingredient : currentIngredients) {
+                    if(!newIngredients.contains(ingredient)) {
+                        canMake = false;
+                        break;
+                    }
+                }
+                if(canMake) {
+                    newIngredients.add(recipes[recipeNum]);
+                    answer.add(recipes[recipeNum]);
+                    toRemove.add(recipeNum);
+                    hasNewIngredients = true;
+                }
+            }
+            for(Integer remove : toRemove)
+                canNotMake.remove(remove);
+        } while(hasNewIngredients);
+        return answer;
+    }
+}

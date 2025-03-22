b73ef4bf0df323209939036887cde097f50c6705
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-22 20:05:59 +0900

    Time: 5 ms (74.55%), Space: 47.4 MB (5.23%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.java
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.java
@@ -1,18 +1,15 @@
 class Solution {
     public int maxSum(int[][] grid) {
         int[][] columm3Sum = new int[grid.length][grid[0].length - 2];
-        int[][] column1Sum = new int[grid.length - 2][grid[0].length - 2];
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[0].length - 2; j++) {
-                if(i != 0 && i != grid.length - 1)
-                    column1Sum[i - 1][j] = grid[i][j + 1];
                 columm3Sum[i][j] = grid[i][j] + grid[i][j + 1] + grid[i][j + 2];
             }
         }
         int max = Integer.MIN_VALUE;
         for(int i = 0; i < grid.length - 2; i++) {
             for(int j = 0; j < grid[0].length - 2; j++) {
-                max = Math.max(max, columm3Sum[i][j] + column1Sum[i][j] + columm3Sum[i + 2][j]);
+                max = Math.max(max, columm3Sum[i][j] + grid[i + 1][j + 1] + columm3Sum[i + 2][j]);
             }
         }
         return max;

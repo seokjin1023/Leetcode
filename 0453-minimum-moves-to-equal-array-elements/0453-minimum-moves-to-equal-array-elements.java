a302bc93cd89214a5058b3c4ba60f2a73afb2c3a
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-22 23:07:50 +0900

    Time: 6 ms (23.72%), Space: 46.4 MB (5.04%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
@@ -1,10 +1,11 @@
+//핵심 아이디어: n 개중 n - 1개에 1을 더할 수 있다는 것 = 자기 자신만 1을 뺄 수 있음.
+//가장 최솟값과 자신만의 차이를 계속 더해나가면 답이 됨.
+//즉, 모든 값을 더하고 최솟값을 n번 곱해서 빼주면 됨.
+
 class Solution {
     public int minMoves(int[] nums) {
-        Arrays.sort(nums);
-        int answer = 0;
-        for(int i = 0; i < nums.length - 1; i++) {
-            answer += (nums[i + 1] - nums[i]) * (nums.length - 1 - i);
-        }
-        return answer;
+        int min = Arrays.stream(nums).min().getAsInt();
+        int sum = Arrays.stream(nums).sum();
+        return sum - min * nums.length;
     }
 }

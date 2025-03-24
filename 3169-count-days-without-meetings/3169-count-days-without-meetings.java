af28ea8f10074fdedf49eaa21838e9c2df888109
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-24 09:34:22 +0900

    Time: 54 ms (20.88%), Space: 100.7 MB (29.97%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.java
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.java
@@ -1,32 +1,22 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
         int work = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> {
-                if (a[0] != b[0]) {
-                    return Integer.compare(a[0], b[0]); 
-                } else {
-                    return Integer.compare(a[1], b[1]); 
-                }
+        Arrays.sort(meetings, (a, b) -> {
+            if (a[0] != b[0]) {
+                return Integer.compare(a[0], b[0]); // [0] 기준 정렬
+            } else {
+                return Integer.compare(a[1], b[1]); // [1] 기준 정렬
             }
-        );
+        });
+        int maxEndMeeting = 0;
         for(int[] meeting : meetings) {
-            pq.add(meeting);
-        }
-        int lastEndMeeting = 0;
-        while(!pq.isEmpty()) {
-            int[] meeting = pq.poll();
             int start = meeting[0], end = meeting[1];
-            int emptyDay = start - lastEndMeeting - 1;
+            int emptyDay = start - maxEndMeeting - 1;
             if(emptyDay > 0) work += emptyDay;
-            if(pq.isEmpty()) {
-                emptyDay = days - Math.max(lastEndMeeting, end);
-                if(emptyDay > 0) work += emptyDay;
-            }
-            else {
-                lastEndMeeting = Math.max(lastEndMeeting, end);
-            }
+            maxEndMeeting = Math.max(maxEndMeeting, end);
         }
+        int emptyDay = days - maxEndMeeting;
+        if(emptyDay > 0) work += emptyDay;
         return work;
     }
 }

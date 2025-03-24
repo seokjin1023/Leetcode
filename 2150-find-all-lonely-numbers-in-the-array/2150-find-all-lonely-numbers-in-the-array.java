307121670d81eff75589c07f45183c0f6fbe1f57
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-24 20:05:58 +0900

    Time: 30 ms (92.75%), Space: 65.4 MB (64.25%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
@@ -1,18 +1,15 @@
 class Solution {
     public List<Integer> findLonely(int[] nums) {
-        Set<Integer> lonelyNum = new HashSet<>();
-        Set<Integer> isPresent = new HashSet<>();
-        for(int num : nums) {
-            boolean isLonely = true;
-            for(int i = -1 ; i <= 1; i++) {
-                if(isPresent.contains(num + i)) {
-                    lonelyNum.remove(num + i);
-                    isLonely = false;
-                }
-            }
-            if(isLonely) lonelyNum.add(num);
-            isPresent.add(num);
+        if(nums.length == 1) return List.of(nums[0]);
+        List<Integer> answer = new ArrayList<>();
+        Arrays.sort(nums);
+        if(nums[1] - nums[0] > 1) answer.add(nums[0]);
+        for(int i = 1; i < nums.length - 1; i++) {
+            if(nums[i] - nums[i - 1] > 1 && nums[i + 1] - nums[i] > 1)
+                answer.add(nums[i]);
         }
-        return new ArrayList<>(lonelyNum);
+        if(nums[nums.length - 1] - nums[nums.length - 2] > 1)
+            answer.add(nums[nums.length - 1]);
+        return answer;
     }
 }

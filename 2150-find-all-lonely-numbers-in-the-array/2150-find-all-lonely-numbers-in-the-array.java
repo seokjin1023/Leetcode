15e647faad1f9b3171238b444bf19bc90ec7a81c
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-24 20:08:45 +0900

    Time: 30 ms (92.75%), Space: 65.4 MB (64.25%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
@@ -3,13 +3,12 @@ class Solution {
         if(nums.length == 1) return List.of(nums[0]);
         List<Integer> answer = new ArrayList<>();
         Arrays.sort(nums);
-        if(nums[1] - nums[0] > 1) answer.add(nums[0]);
-        for(int i = 1; i < nums.length - 1; i++) {
-            if(nums[i] - nums[i - 1] > 1 && nums[i + 1] - nums[i] > 1)
+        for (int i = 0; i < nums.length; i++) {
+            if ((i == 0 || nums[i] - nums[i - 1] > 1) &&
+                (i == nums.length - 1 || nums[i + 1] - nums[i] > 1)) {
                 answer.add(nums[i]);
+            }
         }
-        if(nums[nums.length - 1] - nums[nums.length - 2] > 1)
-            answer.add(nums[nums.length - 1]);
         return answer;
     }
 }

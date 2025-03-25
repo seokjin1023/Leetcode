22e46aceaa6692c9c46cbcde14df68207aae0b2d
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-25 10:37:00 +0900

    Time: 120 ms (28.51%), Space: 143.4 MB (5.7%) - LeetHub

---
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.java
@@ -0,0 +1,39 @@
+//가로와 세로에 같은 함수를 적용해서 찾을 수 있다.
+//어떤 값에서 시작하는게 최대가 존재한다면 그 값을 적용한다.
+//적용된 값에서 어떤 값에 포함된다면 포기..?
+class Solution {
+    public boolean checkValidCuts(int n, int[][] rectangles) {
+        List<int[]> xList = getPosition(true, rectangles);
+        List<int[]> yList = getPosition(false, rectangles);
+        if(getBoundNum(xList) >= 2 || getBoundNum(yList) >= 2)
+            return true;
+        return false;
+    }
+    private List<int[]> getPosition(boolean isX, int[][]rectangles) {
+        List<int[]> list = new ArrayList<>();
+        int start = 1, end = 3;
+        if(isX) {
+            start = 0;
+            end = 2;
+        }
+        for(int[] rectangle : rectangles)
+            list.add(new int[]{rectangle[start], rectangle[end]});
+
+        list.sort(Comparator.comparingInt(a -> a[0]));
+        return list;
+    }
+    private int getBoundNum(List<int[]> positions) {
+        int boundCount = 0;
+        int end = positions.get(0)[1];
+        for(int[] position : positions) {
+            if(position[0] >= end) {
+                boundCount++;
+                end = position[1];
+            }
+            else {
+                end = Math.max(end, position[1]);
+            }
+        }
+        return boundCount;
+    }
+}

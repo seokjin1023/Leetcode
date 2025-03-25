39fc03324c392fb96956c3fa63ca422177677090
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-25 10:47:34 +0900

    Time: 106 ms (39.91%), Space: 122.5 MB (52.19%) - LeetHub

--- 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.java
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.java
@@ -1,6 +1,7 @@
 //가로와 세로에 같은 함수를 적용해서 찾을 수 있다.
 //어떤 값에서 시작하는게 최대가 존재한다면 그 값을 적용한다.
 //start되는 부분이 end보다 크다면 그 중간에 cut을 적용해도 괜찮다!
+//만약 더 효율적으로 바꾸고 싶다면 rectangles를 그대로 getBoundNum에 사용하고 x나 y에 따른 parameter를 추가해서 사용.
 class Solution {
     public boolean checkValidCuts(int n, int[][] rectangles) {
         int[][] xPos = new int[rectangles.length][2];

8fb3e25779c757a9d5518f5bac6e5cef191b5997
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-28 17:14:41 +0900

    Time: 0 ms (100%), Space: 44.6 MB (89.84%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    private List<Integer> answer = new ArrayList<>();
+
+    private void dfs(Node node) {
+        if(node.children == null || node.children.isEmpty()) {
+            answer.add(node.val);
+            return;
+        }
+        for(Node child : node.children) {
+            dfs(child);
+        }
+        answer.add(node.val);
+    }
+    public List<Integer> postorder(Node root) {
+        if(root == null) return new ArrayList<>();
+        dfs(root);
+        return answer;
+    }
+}

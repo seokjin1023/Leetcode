0421a8eeab34223f41d3a3c618eab2696b190949
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-28 22:03:40 +0900

    Time: 1 ms (98.28%), Space: 40.9 MB (67.58%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,7 +1,5 @@
 class Solution {
     public int arrangeCoins(int n) {
-        long k = (long)Math.floor((-1 + Math.sqrt(1 + 8.0 * (long)n)) / 2);
-        if((long)n == (k + 1) * (k + 2) / 2) k++;
-        return (int)k;
+        return (int)Math.floor((-1 + Math.sqrt(1 + 8L * n)) / 2);
     }
 }

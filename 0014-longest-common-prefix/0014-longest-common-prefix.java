cb84b99d5c9ac4682302bed29c419018e1e34937
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-29 00:35:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < strs[0].length(); i++) {
+            boolean isCommon = true;
+            char c = strs[0].charAt(i);
+            for(int j = 1; j < strs.length; j++) {
+                if(strs[j].charAt(i) != c) {
+                    isCommon = false;
+                    break;
+                }
+            }
+            if(isCommon)
+                sb.append(c);
+            else
+                break;
+        }
+        return sb.toString();
+    }
+}

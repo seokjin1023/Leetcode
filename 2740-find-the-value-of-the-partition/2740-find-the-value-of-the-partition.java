0bbc6155e1a169b232cd1c1313883fe7680d84a6
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-29 20:47:04 +0900

    Time: 19 ms (40.74%), Space: 55.9 MB (35.56%) - LeetHub

---
+++ 2740-find-the-value-of-the-partition/2740-find-the-value-of-the-partition.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findValueOfPartition(int[] nums) {
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        for(int i = 1; i < nums.length; i++) {
+            min = Math.min(min, nums[i] - nums[i - 1]);
+        }
+        return min;
+    }
+}

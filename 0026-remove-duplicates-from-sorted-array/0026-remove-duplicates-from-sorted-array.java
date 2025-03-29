52eb875ac15f372d6d247222ada0d555a3cc1d00
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-29 12:54:31 +0900

    Time: 2 ms (11.7%), Space: 45.2 MB (12.71%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,14 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int[] answer = new int[nums.length];
         int index = 0;
         Set<Integer> uniqueNum = new HashSet<>();
         for(int num : nums) {
             if(uniqueNum.add(num))
-                answer[index++] = num;
+                nums[index++] = num;
         }
         int answerSize = uniqueNum.size();
-        System.arraycopy(answer, 0, nums, 0, answerSize);
         return answerSize;
     }
 }
